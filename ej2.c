65e02d8de82f5c94bd0d03d030e02931fe47ebba
Maxi Huebra <104871583+MaxiHbra@users.noreply.github.com>
2024-08-18 16:23:40 -0300

    Validar fecha
    Dados tres números naturales correspondientes a día, mes y año de una fecha, indicar
    si dicha fecha es válida.

---
+++ ej2.c
@@ -0,0 +1,21 @@
+/*Ejercicio 6: Dados tres números naturales correspondientes a día, mes y año de una fecha, indicar si dicha fecha es válida.*/
+#include <stdio.h>
+int main() {
+	int dia,mes,anio; /*Enteros para el ingreso*/
+	printf("Por favor ingresar el dia en numero -> ");
+	scanf("%d",&dia);
+	printf("\nPor favor ingresar el mes en numero -> ");
+	scanf("%d",&mes);
+	printf("\nPor favor ingresar el anio en numero -> ");
+	scanf("%d",&anio);
+	if(anio>1900 && anio<=2024) /*Alternativa para filtrar año*/
+		if(mes>0 && mes<13) /*Alternativa para filtrar mes*/
+			if(dia>0 && dia<31) /*Alternativa para filtrar dia*/
+				printf("\nLa fecha ingresada es valida");
+			else printf("\nFecha no valida");
+		else printf("\nFecha no valida");
+	else printf("\nFecha no valida");
+	return 0;
+}
+
+
